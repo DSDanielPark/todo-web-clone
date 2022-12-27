9ee43492c39eb099d380d8b3ea38e8f69c471ff6
parkminwoo <parkminwoo1991@gmail.com>
2022-12-28 00:15:54 +0900

    feat: java basic check

---
+++ app.js
@@ -0,0 +1,13 @@
+let val = 2
+const val2 = 2.2
+console.log(typeof val == typeof val2)
+console.log(typeof 'hello')
+
+const val3 = 'hello'
+const val4 = "hello"
+console.log(typeof val3 == typeof val4)
+
+const sample = [1,2,3,4]
+console.log(sample[1])
+
+val = 5
