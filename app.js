f30f2033d261d45d90f92b58f286b02ddb1a3762
parkminwoo <parkminwoo1991@gmail.com>
2022-12-29 16:45:06 +0900

    chore: get html  object

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-let val = 2
-const val2 = 2.2
-console.log(typeof val == typeof val2)
-console.log(typeof 'hello')
+// let val = 2
+// const val2 = 2.2
+// console.log(typeof val == typeof val2)
+// console.log(typeof 'hello')
 
-const val3 = 'hello'
-const val4 = "hello"
-console.log(typeof val3 == typeof val4)
+// const val3 = 'hello'
+// const val4 = "hello"
+// console.log(typeof val3 == typeof val4)
 
-const sample = [1,2,3,4]
-console.log(sample[1])
+// const sample = [1,2,3,4]
+// console.log(sample[1])
 
-val = 5
+// val = 5
+
+
+const title = document.getElementById("title")
+
+// console.dir(title)
+
+title.innerText = 'got you!';
+
+console.log(title.id);
+console.log(title.className);
