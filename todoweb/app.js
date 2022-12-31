7df4b1aae207a1343be878aed332585f73bb0d1c
parkminwoo <parkminwoo1991@gmail.com>
2022-12-31 21:42:54 +0900

    feat: prevent defautl of reloadpage from form

--- todoweb/app.js
+++ todoweb/app.js
@@ -8,6 +8,7 @@ const loginButton = loginForm.querySelector("button");
 // const loginButton = document.querySelector("#login-form button");
 
 
+// ########################### 1. js의 form안에 input으로 valid하기 vs js function으로 valid하기
 // function onLoginBtnClick () {
 //     loginInput.value = "from app.js"
 //     console.dir(loginInput);
@@ -15,15 +16,30 @@ const loginButton = loginForm.querySelector("button");
 // }
 
 
-function onLoginBtnClick () {
-    const username = loginInput.value;
-    console.log(username);
-    if (username === ""){
-        alert("please write your name");
-    } else if (username.length > 15){
-        alert("your name is too long. plase under 15");
-    }
-}
+// function onLoginBtnClick () {
+//     const username = loginInput.value;
+//     console.log(username);
+//     if (username === ""){
+//         alert("please write your name");
+//     } else if (username.length > 15){
+//         alert("your name is too long. plase under 15");
+//     }
+// }
 
 
 // loginButton.addEventListener("click", onLoginBtnClick);
+
+
+
+// ########################### 2. onLoginSubmit 및 eventListener 생성해주기
+
+// enter나 클릭해주면 submit해준다.
+function onLoginSubmit(whatisthis) {
+    whatisthis.preventDefault();
+    console.log(whatisthis);
+    
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
