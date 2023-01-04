85a8aef237f375b58fec1fad7d737954b4e72e98
parkminwoo <parkminwoo1991@gmail.com>
2023-01-04 21:47:28 +0900

    feat: show time

--- todoweb/js/clock.js
+++ todoweb/js/clock.js
@@ -2,11 +2,16 @@ const clock = document.querySelector("h2#clock");
 
 // clock.innerText = "clock!!!!!!!!!!" 테스트
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minute = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minute}:${seconds}`
 }
 
 // 5000ms == 5s
-setInterval(sayHello, 5000)
+getClock()
+setInterval(getClock, 1000)
 
 
