f125fb3824dfd537be08d96caa3161b8a90ab26d
parkminwoo <parkminwoo1991@gmail.com>
2023-01-05 22:24:38 +0900

    feat: add bg image and task

--- todoweb/js/background.js
+++ todoweb/js/background.js
@@ -7,3 +7,42 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 document.body.appendChild(bgImage)
 
+
+// 버튼 클릭하면 랜덤으로 색 조합되서 body.background.color변하게 하기
+// https://blog.idrisolubisi.com/how-to-create-beautiful-gradients-with-javascript
+// import "/src/index.js";
+
+// const colors = [
+//   "#ef5777",
+//   "#575fcf",
+//   "#4bcffa",
+//   "#34e7e4",
+//   "#0be881",
+//   "#f53b57",
+//   "#3c40c6",
+//   "#0fbcf9",
+//   "#00d8d6",
+//   "#05c46b",
+//   "#ffc048",
+//   "#ffdd59",
+//   "#ff5e57",
+//   "#d2dae2",
+//   "#485460",
+//   "#ffa801",
+//   "#ffd32a",
+//   "#ff3f34"
+// ];
+
+// const giveMeBtn = document.querySelector("button");
+// var css = document.querySelector("h4");
+
+// function clickGiveMeBtn() {
+//   const color1 = colors[Math.floor(Math.random() * colors.length)];
+//   const color2 = colors[Math.floor(Math.random() * colors.length)];
+
+//   document.body.style.background =
+//     "linear-gradient: " + color1 + ", " + color2 + ")";
+//   css.textContent = document.body.style.background + ";";
+// }
+
+// giveMeBtn.addEventListener("click", clickGiveMeBtn);
