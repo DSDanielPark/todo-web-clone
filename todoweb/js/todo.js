b40fdd64d83224d514615e4cc87120d7933d6090
parkminwoo <parkminwoo1991@gmail.com>
2023-01-08 00:01:05 +0900

    feat: make delete todolist button

--- todoweb/js/todo.js
+++ todoweb/js/todo.js
@@ -3,10 +3,21 @@ const toDoInput = toDoForm.querySelector("input");
 // const ToDoInput = document.querySelector("#todo-form input"); 바로 윗 줄과 동일
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    // console.dir(event.target.parentElement.innerText);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
 function paintToDO(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "X"
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newTodo;
     console.log(li);
     toDoList.appendChild(li);
