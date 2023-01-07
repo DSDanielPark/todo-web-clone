afc98c386d95a22bce84a8dd948746cdc3c4c380
parkminwoo <parkminwoo1991@gmail.com>
2023-01-08 00:13:28 +0900

    feat: toDos to localStorage

--- todoweb/js/todo.js
+++ todoweb/js/todo.js
@@ -3,12 +3,20 @@ const toDoInput = toDoForm.querySelector("input");
 // const ToDoInput = document.querySelector("#todo-form input"); 바로 윗 줄과 동일
 const toDoList = document.getElementById("todo-list");
 
+// todo list를 local storage에 저장해서 새로고침되도 보이게
+const toDos = [];
+
+
 function deleteToDo(event) {
     // console.dir(event.target.parentElement.innerText);
     const li = event.target.parentElement;
     li.remove();
 }
 
+
+function saveToDos() {
+    localStorage.setItem("todos", toDos);
+}
 
 function paintToDO(newTodo) {
     const li = document.createElement("li");
@@ -21,17 +29,16 @@ function paintToDO(newTodo) {
     span.innerText = newTodo;
     console.log(li);
     toDoList.appendChild(li);
-    
 }
-
 
 
 function handleToDoSumit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
-    
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDO(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSumit)
